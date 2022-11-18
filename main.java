f961cf0f2a024ac642d325109f8b6d98334136b8
Jacob Deguara <116016393+JacobDeguara@users.noreply.github.com>
2022-11-18 22:12:13 +0100

    Exception Handling commit

--- main.java
+++ main.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
 
 public class main {
-
     public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
         int result = 0;
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         Board x = new Board(16); //create Board object
         x.addBombs(16, 40); //add the bombs and numbers
         while(result == 0){
             x.printBoardWithShowing(); 
             System.out.println("Enter 2 Numbers (For (x,y) Ex | x y | )");
 
-            int num1 = myObj.nextInt();
-            int num2 = myObj.nextInt();
-            result = x.selectOnBoard(num1, num2); //select the input; if 1(successful), 0(repeat), -1(failer)
+            boolean repeat;
+            do{
+                try{
+                    repeat = false;
+                    num1 = scanner.nextInt();
+                    num2 = scanner.nextInt();
+                } catch(Exception e){
+                    scanner.next();
+                    repeat = true;
+                    System.out.println("You entered the wrong inputs");
+                }
+
+                if(repeat == false){ //this is to avoid any colition if the exeption is detected
+                    if(num1 < 0 || num1 > 16){
+                        repeat = true;
+                        System.out.println("You entered number x beyond the borders");
+                    }
+                    if(num2 < 0 || num2 > 16){
+                        repeat = true;
+                        System.out.println("You entered number y beyond the borders");
+                    }
+                }
+
+            }while(repeat == true); // is only true
+
+            result = x.selectOnBoard(num2, num1); //select the input; if 1(successful), 0(repeat), -1(failer)
             if(result == 1){ //if 1(successful) check if finished
                 result = x.isItFinished();//check if Finish; if 0(not finish so repeat), 1(success finish)
+            }else if(result == 0){
+                System.out.println("You entered numbers already shown (board will be re-printed)");
+                result = x.isItFinished();
             }
         }
         if(result == -1){
@@ -25,5 +52,7 @@ public class main {
             x.printBoard();
             System.out.println("You Did it, you saved America.");
         }
+
+        scanner.close();
         }
     }
